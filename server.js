c40f814bedb084883d7b5fb03f99fdb38711ba9f
Dianna Mack <dianna.mack@yahoo.com>
2020-09-10 22:49:06 -0400

    update port

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require("express");
 const fs = require("fs");
 
 
-// Sets up Express App
+// Sets up Express App and Port
 const PORT = process.env.PORT || 3001;
 const app = express();
 
